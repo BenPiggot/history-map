94c022418b757d9277b30932c1df37f6d256dd9c
ben <wbpiggot@gmail.com>
2016-12-09 15:19:23 -0800

    Adjusting map index page

--- dist/public/js/maps.bundle.js
+++ dist/public/js/maps.bundle.js
@@ -46,7 +46,14 @@
 
 	'use strict';
 
-	console.log('hello world');
+	var mymap = L.map('mapid', { zoomControl: false }).setView([10, -30], 3);
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	    maxZoom: 18,
+	    id: 'mapbox.streets',
+	    accessToken: 'pk.eyJ1IjoiYmVucGlnZ290IiwiYSI6ImYwU2swWkUifQ.MJDSGs4FaCV1GlurP-nIDA'
+	}).addTo(mymap);
 
 /***/ }
 /******/ ]);
